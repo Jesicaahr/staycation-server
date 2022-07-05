7f1a2f0b876764f187641a9696e8fca161d1ebac
jesicaahr <jesicaprilia18@gmail.com>
2022-07-05 16:12:43 +0700

    feat/auth-for-login

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -15,10 +15,16 @@ module.exports = {
             const alertMessage = req.flash('alertMessage');
             const alertStatus = req.flash('alertStatus');
             const alert = { message: alertMessage, status: alertStatus };
-            res.render('index', { 
-                alert,
-                title: 'Staycation | Login'
-             });
+            if(req.session.user == null || req.session.user == undefined) {
+                res.render('index', { 
+                    alert,
+                    title: 'Staycation | Login'
+                });
+                
+            }
+            else {
+                res.redirect('/admin/dashboard');
+            }
             
         } catch (error) {
             req.flash('alertMessage', `${error.message}`)
@@ -41,7 +47,14 @@ module.exports = {
                 req.flash('alertStatus', 'danger')
                 res.redirect('/admin/signin');
             }
-            res.redirect('/admin/dashboard')
+
+            req.session.user = {
+                id: user.id,
+                username: user.username
+            }
+
+            res.redirect('/admin/dashboard');
+
         } catch (error) {
             res.redirect('/admin/signin');
         }
