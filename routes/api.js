d9300f73348a3bcff07fd2efb950d4bb9dab195f
jesicaahr <jesicaprilia18@gmail.com>
2022-07-10 17:38:16 +0700

    feat/api-detail-page

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const { upload, uploadMultiple } = require('../middlewares/multer')
 const auth = require('../middlewares/auth')
 
 router.get('/landing-page', apiController.landingPage)
+router.get('/detail-page/:id', apiController.detailPage)
 
 module.exports = router
