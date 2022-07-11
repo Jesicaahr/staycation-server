0eb546402df57e04131043c018c3923cc6b10063
jesicaahr <jesicaprilia18@gmail.com>
2022-07-11 16:38:27 +0700

    feat/api-booking

--- routes/api.js
+++ routes/api.js
@@ -5,5 +5,6 @@ const auth = require('../middlewares/auth')
 
 router.get('/landing-page', apiController.landingPage)
 router.get('/detail-page/:id', apiController.detailPage)
+router.post('/booking-page/', upload, apiController.bookingPage) 
 
 module.exports = router
